7a407d7ed69e840f20a9f085bd0fdd1d3ce599d0
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2021-09-06 21:00:09 +0530

    Add files via upload

---
+++ c++ solution/count-nodes-of-linked-list.cpp
@@ -0,0 +1,15 @@
+int getCount(struct Node* head){
+    Node * current_node = head; 
+    if(current_node == NULL)
+        return 0; 
+    else
+    {
+        int counter=0; 
+        while(current_node != NULL)
+        {
+            counter++;
+            current_node = current_node -> next; 
+        }
+        return counter;
+    }
+}
